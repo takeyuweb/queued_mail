c8fa9779b1d05a59a9f8986b676ed81d4426a951
Yuichi Takeuchi <uzuki05@takeyu-web.com>
2012-09-06 18:51:01 +0900

    整理

--- lib/queued_mail.rb
+++ lib/queued_mail.rb
@@ -1,4 +1,8 @@
 require "queued_mail/engine"
 
 module QueuedMail
+  autoload :DeliveryMethod,  'queued_mail/delivery_method'
+  autoload :Mailer,          'queued_mail/mailer'
+  autoload :DeliverEmailJob, 'queued_mail/deliver_email_job'
 end
+
